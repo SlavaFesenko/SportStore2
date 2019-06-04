7b3efe32b29ebaf74029c26b89ace8c4b6b87dd5
Slava Fesenko <fvsleader@gmail.com>
2019-06-04 13:00:17 +0300

    pagination helper

--- SportStore2/SportStore2/Controllers/ProductController.cs
+++ SportStore2/SportStore2/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportStore2.Helpers;
 using SportStore2.Models;
 using SportStore2.Models.RepositoryItems;
 using System;
@@ -11,13 +12,29 @@ namespace SportStore2.Controllers
     public class ProductController : Controller
     {
         private IProductRepository _repository;
+        /// <summary>
+        /// Elements per page
+        /// </summary>
+        private int _pageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             _repository = repo;
         }
 
-        public ViewResult List() => View(_repository.Products);
+        /// <summary>
+        /// https://localhost:port_num/Product/List/?pageNum=2
+        /// Get the list of products with pagination
+        /// </summary>
+        /// <param name="pageNum">Number of page to show</param>
+        /// <returns></returns>
+        public ViewResult List(int pageNum = 1)
+        {
+            var sortedProducts = _repository.Products.OrderBy(p => p.ProductID);
+            var paginatedProducts = PaginationHelper.Paginate(sortedProducts, pageNum, _pageSize);
+
+            return View(paginatedProducts);
+        } 
 
     }
 }
---
+++ SportStore2/SportStore2/Helpers/PaginationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore2.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static IQueryable<T> Paginate<T>(IQueryable<T> sortedItems, int pageNum, int pageSize)
+        {
+            // calculate the count of products to skip
+            var skippedList = sortedItems.Skip((pageNum - 1) * pageSize);
+
+            // from the cut list after skip take the first {_pageSize} products
+            var neededPortion = skippedList.Take(pageSize);
+
+            return neededPortion;
+        }
+    }
+}
